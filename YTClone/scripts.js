241240683faf1d8b1fe4d8da72719195928e3b12
yadav-deepakk <deepakyadav53882@gmail.com>
2023-11-19 23:59:02 +0530

    changes in sidebar and directory structure.

--- YTClone/scripts.js
+++ YTClone/scripts.js
@@ -1,7 +1,10 @@
-// alert("Scripts invoked!!");
-window.onload = () =>  {
+window.onload = () => {
 	let hamburgerMenu = document.querySelector(".nav-left .fa-bars");
 	hamburgerMenu.addEventListener("click", toggleSideBar);
+
+	let currentDate = new Date(); 
+	let htmlElementsYear =  document.querySelector(".current-year");
+	htmlElementsYear.innerHTML = currentDate.getFullYear(); 
 };
 
 let toggleSideBar = () => {
@@ -14,15 +17,16 @@ let toggleSideBar = () => {
 	sideDrawer.classList.toggle("d-none");
 };
 
-let selectOption = (e) => {
-	console.log("e: " + e);
-	if(e.classList.contains("active-option")) return; 
-	let opts = e.parentNode.children; 
+let selectOption = (option) => {
+	console.log("option: " + option);
+	console.log("option dataset: " + option.dataset.id);
+	if(option.classList.contains("active-option")) return; 
+	let opts = option.parentNode.children; 
 	console.log("opts: " + opts);
 	for (var i=0; i<opts.length; i++) {
 		if(opts[i].classList.contains("active-option")){
 			opts[i].classList.remove("active-option"); 
 		}
 	}
-	e.classList.toggle("active-option");
+	option.classList.toggle("active-option");
 };
